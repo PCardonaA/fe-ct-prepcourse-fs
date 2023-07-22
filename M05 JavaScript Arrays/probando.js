d5ff36b0a3c3340f95919b4614b1324712f5af62
PCardonaA <pacardonaar@unal.edu.co>
2023-07-21 22:31:48 -0500

    Test 05 Done

---
+++ M05 JavaScript Arrays/probando.js
@@ -0,0 +1,11 @@
+function tablaDelSeis() {
+  // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+  // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
+  // Tu código:
+  var tablaDelSeis=[];
+  for(var i =0; i < 11; i++){
+    tablaDelSeis.push(6*i);
+  }
+  console.log(tablaDelSeis);
+}
+tablaDelSeis();
